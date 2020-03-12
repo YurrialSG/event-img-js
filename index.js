ee37517d6a986afa3cf69354f1bcc7e2af0ed694
YurrialSG <yuriinternacional86@gmail.com>
2020-03-12 17:53:48 -0300

    ação ao passar mouse sobre imgs com class e id

---
+++ index.js
@@ -0,0 +1,38 @@
+var userSelection = document.getElementsByClassName('texto');
+var userSelectionImage = document.getElementsByClassName('image');
+
+
+for (var i = 0; i < userSelectionImage.length; i++) {
+    (function (index) {
+        userSelectionImage[index].addEventListener("mouseover", function () {
+            userSelectionImage[index].style.backgroundColor = "red";
+        })
+
+        userSelectionImage[index].addEventListener("mouseout", function () {
+            userSelectionImage[index].style.backgroundColor = "white";
+        })
+    })(i);
+}
+
+for (var i = 0; i < userSelection.length; i++) {
+    (function (index) {
+        userSelection[index].addEventListener("mouseover", function () {
+            userSelection[index].style.color = "blue";
+        })
+
+        userSelection[index].addEventListener("mouseout", function () {
+            userSelection[index].style.color = "black";
+        })
+    })(i);
+}
+
+document.getElementById("demo").addEventListener("mouseover", mouseOver);
+document.getElementById("demo").addEventListener("mouseout", mouseOut);
+
+function mouseOver() {
+    document.getElementById("demo").style.color = "red";
+}
+
+function mouseOut() {
+    document.getElementById("demo").style.color = "black";
+}
